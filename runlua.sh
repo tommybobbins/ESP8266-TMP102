293ba5dafb121e137d7f7e4f86d23e78be0bc7b3
Tim Gibbon <tng@chegwin.org>
2015-11-29 16:32:58 +0000

    Code refactor. Power saving

--- runlua.sh
+++ runlua.sh
@@ -1,3 +1,5 @@
-luatool.py --src httppost.lua
-luatool.py --src tmp102.lua
-luatool.py --src init_bluejelly.lua --dest init.lua -r
+#!/bin/bash
+PORT=$1
+luatool.py --port ${PORT} --src httppost_bluejelly.lua --dest httppost.lua
+luatool.py --port ${PORT} --src tmp102.lua
+luatool.py --port ${PORT} --src init.lua --dest init.lua -r
--- wipe.sh
+++ wipe.sh
@@ -1,1 +1,13 @@
-luatool.py -w -r
+#!/bin/bash
+PORT=$1
+if [ -e ${PORT} ]
+then
+  while true
+    do
+      luatool.py --port ${PORT} -w -r
+      sleep 0.1
+    done
+else 
+   echo "Unable to find the USB connection"
+   exit 2
+fi
